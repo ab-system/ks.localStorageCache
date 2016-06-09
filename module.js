7866524bd9e99e3f70ed030a8e31de2c4e8e4e8d
egor ol't <olt.egor@gmail.com>
2016-06-09 20:47:35 +0300

    localStorageCache

---
+++ module.js
@@ -0,0 +1,38 @@
+/**
+ * Created by eolt on 08.10.2015.
+ */
+
+    angular.module('components', ['localStorageCache'])
+        .controller('mainCtrl', [ '$scope', 'localStorageCacheFactory', '$cacheFactory', function($scope, localStorageCacheFactory, $cacheFactory) {
+
+            var memCache = $cacheFactory('memCache');
+            var lsCache = localStorageCacheFactory('lsCache');
+
+            var memCacheInfo = memCache.info();
+
+            $scope.lsCacheInfo = lsCache.info();
+
+            $scope.addItems = function (count) {
+                var size = lsCache.info().size;
+                for (var i = 0; i < count; i++) {
+                    var key = size + i;
+                    lsCache.put('' + key, i);
+                }
+                $scope.lsCacheInfo = lsCache.info();
+            }
+
+            $scope.getItem = function (key) {
+
+                var value = lsCache.get(key);
+
+                if(value) {
+                    $scope.itemInfo = {
+                        key: key,
+                        value: value
+                    };
+                }
+                else {
+                    $scope.itemInfo = null;
+                }
+            }
+        }]);
